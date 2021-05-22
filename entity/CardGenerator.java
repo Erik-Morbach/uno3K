b33ee2dcec9d0b377d1ba4a69b34d724c9da0660
Cassimiroardo <edu27.cassimiro@gmail.com>
2021-05-21 22:37:28 -0300

    inicio do menu + geradores de mão e carta

--- entity/CardGenerator.java
+++ entity/CardGenerator.java
@@ -1,11 +1,24 @@
 package entity;
+import java.util.Iterator;
 import java.util.Random;
+
+import enums.CardColor;
 
 public class CardGenerator {
     public static Card execute() {
         Random random = new Random();
-        int value = random.nextInt(100);
-        // para implementações futuras
-        return new CommonCard();
+        int rndCard = random.nextInt(4);
+        
+        CardColor[] colors = CardColor.values();
+        CardColor color = CardColor.RED; // default
+        
+        for (int i = 0; i < rndCard; i++) {
+        	color = colors[i];
+		}
+        
+        int rndNumber = random.nextInt(9);
+        
+        // para implementaï¿½ï¿½es futuras
+        return new CommonCard(rndNumber, color);
     }
 }
--- enums/CardColor.java
+++ enums/CardColor.java
@@ -1,11 +1,15 @@
 package enums;
 
 public enum CardColor {
-        BLACK("\u001B[30m"), RED("\u001B[31m"), GREEN("\u001B[32m"), 
-        YELLOW("\u001B[33m"), BLUE("\u001B[34m"), RESET("\u001B[35m");
+        RED("VERMELHO"), 
+        GREEN("VERDE"), 
+        YELLOW("AMARELO"), 
+        BLUE("AZUL"); 
+        
 		public String getValue() { return value; }
 
 		private final String value;
+		
 		CardColor(String string) { 
 			value = string;
 			// TODO Auto-generated constructor stub 
