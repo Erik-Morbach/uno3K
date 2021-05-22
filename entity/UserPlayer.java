ec046a27df4f65e5753b7f09d78cdb383ae50fad
Erik Morbach <morbacherik@gmail.com>
2021-05-22 00:42:08 -0300

    Pequenos Updates

--- entity/UserPlayer.java
+++ entity/UserPlayer.java
@@ -1,5 +1,7 @@
 package entity;
 
+import enums.Action;
+
 public class UserPlayer extends Player {
 	public UserPlayer() {
 		super();
@@ -14,13 +16,14 @@ public class UserPlayer extends Player {
 		// card = menu.getValidOption();
 		// perguntaQualCarta()
 		// pegaResposta()
-		Card card = null;
+		Menu.handleDisplayUserCards();
+		Card card = getCard();
 		
 		if(card!=null) 
 			this.hand.discartCard(card);
 
+		
 		return card;	
-		// Show hand to menu
 	}
 	
 
@@ -35,12 +38,72 @@ public class UserPlayer extends Player {
 	@Override
 	public void play() {
 		// TODO Auto-generated method stub
-		// pergunto()
-		// pegaResposta(2)
-		if(discartAction()!=null) 
-			return;
+		Menu.handleActionDisplay(null);
 		
-		buyAction();
+		Action action = getAction();
+		switch(action) {
+		case DISCART:
+			discartAction();
+			showAction(Action.DISCART);
+			break;
+		case BUY_IT:
+			buyAction();
+			showAction(Action.BUY_IT);
+			break;
+		}
+	}
+	private Action getAction() {
+		int option;
+		while(true) {
+			try {
+				option = Menu.scanner.nextInt();
+				switch(option) {
+					case 1:
+						if(validate(Action.DISCART))
+							return Action.DISCART;
+						
+						break;
+					case 2:
+						if(validate(Action.BUY_IT))
+							return Action.BUY_IT;
+						break;
+				}
+			}catch(Error e) {
+			}
+			errorMessage();
+		}
+	}
+	private boolean validate(Action action) {
+		Card center = Game.getInstance().getCenterCard();
+		
+		boolean valid = this.hand.getValid(center)!=null;
+		
+		if(action==Action.DISCART) return valid;
+		
+		return !valid;
+	}
+	private Card getCard() {
+		String option;
+		while(true) {
+			try {
+				option = Menu.scanner.next();
+			
+				if(option.length()>1) {
+					errorMessage();
+					continue;
+				}
+				char value = option.charAt(0);
+				if(value>='A' && value<='Z') {
+					int number = value-'A';
+					return this.hand.getCards().get(number);
+				}
+			}catch(Error e) {
+			}
+			errorMessage();
+		}
+	}
+	private void errorMessage() {
+		System.out.println("Tente novamente!");
 	}
 	
 }
